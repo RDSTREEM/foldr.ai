acaf49cd95d8a725991d096ecd2e3fbe55e5c692
RDSTREEM <nahomabebe2002@gmail.com>
2025-07-17 23:13:21 +0300

    A post build process to auto add the rest of the required files to /dist

---
+++ copy-static.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const path = require("path");
+
+const filesToCopy = ["manifest.json", "style.css"];
+const distDir = path.join(__dirname, "dist");
+
+if (!fs.existsSync(distDir)) fs.mkdirSync(distDir);
+
+for (const file of filesToCopy) {
+  const src = path.join(__dirname, file);
+  const dest = path.join(distDir, file);
+  if (fs.existsSync(src)) {
+    fs.copyFileSync(src, dest);
+    console.log(`Copied ${file} to dist/`);
+  }
+}
